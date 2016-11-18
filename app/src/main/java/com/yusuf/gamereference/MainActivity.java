a8b263ec931b8b2b266bbba0344831ba4b648016
Yusuf Qedan <yusuf9191@gmail.com>
2016-11-18 09:04:41 -0800

    add button to go to new activity, add butterknife to bind the button

--- app/src/main/java/com/yusuf/gamereference/MainActivity.java
+++ app/src/main/java/com/yusuf/gamereference/MainActivity.java
@@ -1,13 +1,28 @@
 package com.yusuf.gamereference;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.button) Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, GameSearchActivity.class);
+        startActivity(intent);
     }
 }
