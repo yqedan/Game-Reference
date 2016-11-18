63ffe724a8e7b7f38720c787f29ef69aba12a171
Yusuf Qedan <yusuf9191@gmail.com>
2016-11-18 09:47:37 -0800

    remove GridView and decide to use a ListView instead, add ArrayAdapter and seed data for ListView

--- app/src/main/java/com/yusuf/gamereference/GameSearchActivity.java
+++ app/src/main/java/com/yusuf/gamereference/GameSearchActivity.java
@@ -2,12 +2,29 @@ package com.yusuf.gamereference;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class GameSearchActivity extends AppCompatActivity {
+    @Bind(R.id.listView) ListView mListView;
+    String[] mGames = new String[] {
+            "The Legend of Zelda",
+            "The Legend of Zelda: The Wind Waker",
+            "The Legend of Zelda: Link's Awakening",
+            "The Legend of Zelda: Oracle of Ages",
+            "The Legend of Zelda: Ocarina of Time",
+            "Zelda II: The Adventure of Link",
+            "The Legend of Zelda: A Link to the Past"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_search);
+        ButterKnife.bind(this);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mGames);
+        mListView.setAdapter(adapter);
     }
 }
