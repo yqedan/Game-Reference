aff428c4c186a0d1a42f6a9f6350fcf1ab60f5be
Yusuf Qedan <yusuf9191@gmail.com>
2016-11-18 12:01:12 -0800

    add another icon to MainActivity, add background color

--- app/src/main/java/com/yusuf/gamereference/GameDetailActivity.java
+++ app/src/main/java/com/yusuf/gamereference/GameDetailActivity.java
@@ -3,16 +3,22 @@ package com.yusuf.gamereference;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class GameDetailActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class GameDetailActivity extends AppCompatActivity {
+    @Bind(R.id.textView) TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_detail);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         String game = intent.getStringExtra("game");
         Toast.makeText(GameDetailActivity.this, game, Toast.LENGTH_LONG).show();
+        mTextView.setText(game);
     }
 }
