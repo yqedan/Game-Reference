1d3125c859a42b1f5e356a7b44d5d1bf64bdce04
Yusuf Qedan <yusuf9191@gmail.com>
2016-12-09 10:30:24 -0800

    add ability to login, add logout button to game search activity

--- app/src/main/java/com/yusuf/gamereference/ui/GameSearchActivity.java
+++ app/src/main/java/com/yusuf/gamereference/ui/GameSearchActivity.java
@@ -1,11 +1,15 @@
 package com.yusuf.gamereference.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.yusuf.gamereference.R;
 import com.yusuf.gamereference.adapters.GameListAdapter;
 import com.yusuf.gamereference.models.Game;
@@ -39,6 +43,7 @@ public class GameSearchActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         final String search = getIntent().getStringExtra("search");
         setTitle(search);
+        //TODO add loading progress dialog
         getGames(search);
 
         mAdapter = new GameListAdapter(getApplicationContext(), mGames);
@@ -63,12 +68,38 @@ public class GameSearchActivity extends AppCompatActivity {
                 }
 
                 if ( !loading && (pastVisibleItems + visibleItemCount >= totalItemCount) ) {
+                    //TODO add loading progress dialog and prevent extraneous api call if there are no remaining pages
                     Log.d(TAG, "End is reached!");
                     getGames(search);
                     loading = true;
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_logout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                Log.d(TAG, "logging out");
+                logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(GameSearchActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     private void getGames(String title){
@@ -80,11 +111,13 @@ public class GameSearchActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
+                //TODO detect if the are no results on first page search
                 mGames.addAll(GameService.processSearch(response));
                 GameSearchActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         mAdapter.notifyDataSetChanged();
+                        //TODO remove loading progress dialog
                     }
                 });
             }
